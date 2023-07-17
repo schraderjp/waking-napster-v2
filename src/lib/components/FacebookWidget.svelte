9d05f3cc30e09d70cbbf8f1786fb7bf8ed0b870a
= <=>
2023-07-17 17:23:33 -0400

    attempting to make Facebook widget work

--- src/lib/components/FacebookWidget.svelte
+++ src/lib/components/FacebookWidget.svelte
@@ -1,1 +1,15 @@
-<div class="fb-page" data-href="https://www.facebook.com/wakingnapster" data-tabs="events" data-width="400" data-height="600" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/wakingnapster" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/wakingnapster">Waking Napster</a></blockquote></div>
+<div
+	class="fb-page"
+	data-href="https://www.facebook.com/wakingnapster"
+	data-tabs="events"
+	data-width="400"
+	data-height="600"
+	data-small-header="false"
+	data-adapt-container-width="true"
+	data-hide-cover="false"
+	data-show-facepile="true"
+>
+	<blockquote cite="https://www.facebook.com/wakingnapster" class="fb-xfbml-parse-ignore">
+		<a href="https://www.facebook.com/wakingnapster">Waking Napster</a>
+	</blockquote>
+</div>
