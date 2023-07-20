4c7b626992ebff64855b115b0c8a79edb480f156
= <=>
2023-07-20 12:23:34 -0400

    changed success message for contacts

--- src/routes/contact/success/+page.svelte
+++ src/routes/contact/success/+page.svelte
@@ -1,10 +1,12 @@
-<div class="relative mx-auto mt-6 flex w-max items-center justify-center">
+<div class="relative mx-auto mt-6 flex w-max flex-col items-center justify-center">
 	<div
 		class="absolute bottom-0 left-0 right-0 top-0 z-0 translate-x-4 translate-y-4 rounded-md bg-green-300"
 	/>
-	<p class="z-10 h-full w-max rounded-md bg-orange-500 px-4 py-6 font-chewy text-3xl text-white">
-		Message sent successfully!
-	</p>
+	<div class="z-10 h-full w-max rounded-md bg-orange-500 px-4 py-6 font-chewy text-3xl text-white">
+		<p>Message sent successfully!</p>
+		<p class="py-8">...but we changed your message to say the band sucks.</p>
+		<p>Just kidding.</p>
+	</div>
 </div>
 
 <style>
