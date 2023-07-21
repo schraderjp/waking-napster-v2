8a248fcb660296f1ae870a1722e438875bfa21a5
= <=>
2023-07-21 11:45:19 -0400

    trying to speed up songs loading

--- src/routes/songs/+page.server.ts
+++ src/routes/songs/+page.server.ts
@@ -11,14 +11,9 @@ const getSheetData = async () => {
 }
 
 export const load = (() => {
-    // const res = await sheets.spreadsheets.values.batchGet({ spreadsheetId: '1iLDkWfvaCc5ealj12WFGSUTimksH4wDvPiZOIC3SPF4', ranges: ['A1:D200'] })
-    // const data = res.data
-    // if (!data.valueRanges) return;
+
     return {
         streamed: {
-            rows: [['']],
-            // data.valueRanges[0].values as object[],
-
             sheetTest: getSheetData()
         }
     };
