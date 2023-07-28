293cd8efb6dd168a35b10675c76f44ae24f312ea
= <=>
2023-07-28 12:14:27 -0400

    changed banner, added mobile variant of banner

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,13 @@
 	import Youtube from 'svelte-youtube-embed';
 	import MdPlayCircleOutline from 'svelte-icons/md/MdPlayCircleOutline.svelte';
 	import banner from '$lib/assets/Waking Napster Banner 2.png';
+	import jayImg from '$lib/assets/jay.jpeg';
+	import aubreyImg from '$lib/assets/aubrey.jpeg';
+	import aliciaImg from '$lib/assets/alicia.jpeg';
+	import daveImg from '$lib/assets/dave.jpeg';
 	import wakingNapster from '$lib/assets/WakingNapster.svg';
+	import mobileBanner from '$lib/assets/wn mobile banner.png';
+	import desktopBanner from '$lib/assets/wn desktop banner 2.png';
 	import closeEyesFace from '$lib/assets/WakingNapster6_01_closed.png';
 	import FaFacebook from 'svelte-icons/fa/FaFacebook.svelte';
 	import FaYoutube from 'svelte-icons/fa/FaYoutube.svelte';
@@ -13,17 +19,28 @@
 	<title>Waking Napster</title>
 	<meta name="description" content="90s cover band in Culpeper, VA" />
 </svelte:head>
+
 <div class="relative mx-auto mt-4 flex w-full items-center justify-center">
+	<img class="md:hidden" src={mobileBanner} alt="Main Banner" />
+	<img class="hidden md:block" src={desktopBanner} alt="Main Banner" />
+	<!-- <div class="mx-auto flex max-w-[100vw]">
+		<img class="w-1/4 flex-shrink" src={aubreyImg} alt="Aubrey" />
+		<img class="w-1/4 flex-shrink" src={daveImg} alt="Dave" />
+		<img class="w-1/4 flex-shrink" src={jayImg} alt="Jay" />
+		<img class="w-1/4 flex-shrink" src={aliciaImg} alt="Alicia" />
+	</div>
 	<img class="mx-auto w-full brightness-50" src={banner} alt="Band banner" />
 	<div
-		class="banner absolute bottom-0 left-0 right-0 top-0 flex h-full items-center rounded-lg bg-gray-700 bg-opacity-50 p-8 font-chewy text-yellow-400 xs:text-2xl sm:text-3xl md:text-4xl lg:text-5xl"
+		class="banner absolute bottom-0 left-0 right-0 top-0 flex h-full items-end justify-center rounded-lg bg-gray-700 bg-opacity-50 p-1 font-chewy text-yellow-400 xs:text-2xl sm:text-3xl md:text-4xl lg:text-5xl"
 	>
 		<img class="h-3/4" src={wakingNapster} alt="Waking Napster large logo" />
-		<p class="text-sx text-center">Playing your favorite pirated songs from the 90's and beyond</p>
-	</div>
+	</div> -->
 </div>
+<!-- <p class="w-full bg-purple-600 px-2 py-2 text-center font-chewy text-yellow-300">
+	Playing your favorite pirated songs from the 90's and beyond
+</p> -->
 <div
-	class="relative -z-10 mx-auto mt-4 flex flex-col items-center justify-center gap-x-2 gap-y-2 p-4 md:max-w-full md:flex-row md:justify-around lg:max-w-full"
+	class="relative -z-10 mx-auto flex flex-col items-center justify-center gap-x-2 gap-y-2 p-4 md:max-w-full md:flex-row md:justify-around lg:max-w-full"
 >
 	<div
 		class="relative -z-10 mt-2 flex max-w-[min(95vw,48rem)] flex-shrink flex-grow -rotate-1 items-center justify-center p-2"
