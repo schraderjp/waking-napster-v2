afec984c03054224c4d3c86021eb661a8b189628
James Schrader <schraderjp@gmail.com>
2023-07-30 13:22:34 -0400

    added transitions on scoll

---
+++ src/lib/components/AnimateOnScroll.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import IntersectionObserver from 'svelte-intersection-observer';
+
+	export let element: HTMLElement;
+	let intersecting: boolean;
+</script>
+
+<IntersectionObserver {element} bind:intersecting>
+	<slot />
+</IntersectionObserver>
