0fb7674d22233de8a7f55b03b98cdfc42a86f69c
James Schrader <schraderjp@gmail.com>
2023-08-04 16:17:08 -0400

    fixed success page

--- src/routes/contact/success/+page.svelte
+++ src/routes/contact/success/+page.svelte
@@ -1,15 +1,12 @@
 <script>
 </script>
 
-<div class="relative mx-auto mt-6 flex w-max flex-col items-center justify-center">
-	<div
-		class="absolute bottom-0 left-0 right-0 top-0 z-0 translate-x-4 translate-y-4 rounded-md bg-green-300"
-	/>
-	<div class="z-10 h-full w-max rounded-md bg-orange-500 px-4 py-6 font-chewy text-3xl text-white">
-		<p>Message sent successfully!</p>
-		<p class="py-8">...but we changed your message to say the band sucks.</p>
-		<p>Just kidding.</p>
-	</div>
+<div
+	class="relative mx-auto mt-6 flex max-w-[min(90vw,24rem)] flex-col items-center justify-center rounded-lg bg-orange-500 p-4 text-center font-chewy text-2xl text-white"
+>
+	<p>Message sent successfully!</p>
+	<p class="py-8">...but we changed your message to say the band sucks.</p>
+	<p>Just kidding.</p>
 </div>
 
 <style>
